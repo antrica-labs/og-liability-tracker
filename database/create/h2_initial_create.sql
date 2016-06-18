b384bba3676fafb661a0943aa88d778ae00454a9
Simon Twogood <simon.twogood@gmail.com>
2016-06-18 08:45:02 -0600

    Moved to H2 database
    SQLite through JDBC has some issues that I would rather not deal with.
    H2 also gives a better migration path to Oracle through it’s
    compatibility modes.

---
+++ database/create/h2_initial_create.sql
@@ -0,0 +1,37 @@
+CREATE TABLE provinces (
+  id IDENTITY PRIMARY KEY,
+  name VARCHAR(255),
+  short_name VARCHAR(2)
+);
+
+CREATE TABLE entity (
+  id IDENTITY PRIMARY KEY,
+  province_id INTEGER,
+  licence VARCHAR(255),
+  location_identifier VARCHAR(255)
+);
+
+CREATE TABLE entity_info (
+  id IDENTITY PRIMARY KEY,
+  entity_id INTEGER,
+  report_month DATE,
+  entity_status VARCHAR(255),
+  calculation_type VARCHAR(255),
+  asset_value DOUBLE DEFAULT 0,
+  liabilty_value DOUBLE DEFAULT 0,
+  abandonment_basic DOUBLE DEFAULT 0,
+  abandonment_additional_event DOUBLE DEFAULT 0,
+  abandonment_gwp DOUBLE DEFAULT 0,
+  abandonment_gas_migration DOUBLE DEFAULT 0,
+  abandonment_vent_flow DOUBLE DEFAULT 0,
+  abandonment_site_specific DOUBLE DEFAULT 0,
+  reclamation_basic DOUBLE DEFAULT 0,
+  reclamation_site_specific DOUBLE DEFAULT 0
+);
+
+CREATE TABLE disposed_entities (
+  id IDENTITY PRIMARY KEY,
+  entity_id INTEGER,
+  effective_date DATE,
+  created_date DATE
+)
